93eadaa06aa0303ca8a75bf41c84430e52c10849
Aleksandar Dobrinic <aleksandar.dobrinic@predavaci.algebra.hr>
2024-06-03 21:13:53 +0200

    Dan 9 - forme

--- 8.superglobalne_varijable/forma.php
+++ 8.superglobalne_varijable/forma.php
@@ -1,18 +1,40 @@
+<?php
+
+$podaci = $_POST;
+
+// var_dump($_POST);
+
+if( !empty($podaci) ){
+    echo 'Kliknuto na Posalji<br><br>';
+
+    $ime = $podaci['first_name'];
+    $prezime = $podaci['last_name'];
+
+    echo "Vase ime je $ime a przime $prezime.";
+
+}else{
+    echo 'Nije Kliknuto na Posalji';
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
 
     <h2>HTML Forms</h2>
 
-    <form action="/action_page.php">
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" value="John"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname" value="Doe"><br><br>
-        <input type="submit" value="Submit">
-    </form> 
+    <form method="POST">
 
-    <p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+        <label for="first_name">Ime:</label><br>
+        <input type="text" id="first_name" name="first_name"><br>
+
+        <label for="last_name">Prezime:</label><br>
+        <input type="text" id="last_name" name="last_name"><br><br>
+
+        <input type="submit" value="Pošalji">
+
+    </form> 
 
 </body>
 </html>
