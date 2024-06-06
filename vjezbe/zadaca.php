d6ff446d21c862496ccb0779e31f50727c8808ca
Aleksandar Dobrinic <aleksandar.dobrinic@predavaci.algebra.hr>
2024-06-06 20:57:03 +0200

    dan 10 - cookie & session & ispit

--- vjezbe/zadaca.php
+++ vjezbe/zadaca.php
@@ -8,12 +8,55 @@
 // Provjerite je li poslana datoteka slika
 // Pohranite poslanu datoteku.
 
-// 2.
-// Dovrsiti zadatak tecajnica.php
-// napravit web formu za konverziju EUR u USD
-// korisink u formu upisuje iznos u eurima i nakon submita
-// vi ispisujete konvertiranu vrijdnost
+if ($_FILES && isset($_FILES["datoteka"])) {
+    // Provjeri je li datoteka poslana
+    if ($_FILES["datoteka"]["error"] === UPLOAD_ERR_OK) {
+        // Kreiraj novu mapu ako ne postoji
+        $direktorij = 'uploads/';
+        if (!file_exists($direktorij)) {
+            mkdir($direktorij, 0777, true);
+        }
 
-// 3.
-// Napraviti kalkulator koji koristi 4 osnovne
-// matematicke funkcije koristenjem web formi
+
+        // Provjeri je li poslana datoteka slika
+        // exif_imagetype() // -> Stjepan
+        $dozvoljeni_formati = array('jpg', 'jpeg', 'png', 'PNG', 'gif', 'webp');
+        $datoteka_info = pathinfo($_FILES['datoteka']['name']);
+        $format = strtolower($datoteka_info['extension']);
+
+        
+        if (in_array($format, $dozvoljeni_formati)) {
+            // Pohrani poslanu datoteku
+            $nova_datoteka = $direktorij . basename($_FILES['datoteka']['name']);
+            if (move_uploaded_file($_FILES['datoteka']['tmp_name'], $nova_datoteka)) {
+                echo 'Datoteka uspješno pohranjena.';
+            } else {
+                echo 'Došlo je do problema prilikom pohrane datoteke.';
+            }
+        } else {
+            echo 'Dozvoljeni formati datoteka su: jpg, jpeg, png, gif';
+        }
+    } else {
+        echo 'Nije poslana datoteka ili je došlo do greške prilikom prijenosa.';
+    }
+} else {
+    echo 'Nije poslan $_FILES niz ili datoteka nije pravilno postavljena.';
+}
+
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Obrazac za upload datoteke</title>
+</head>
+<body>
+    <form method="post" enctype="multipart/form-data">
+        Odaberite datoteku: <input type="file" name="datoteka">
+        <input type="submit" value="Pošalji">
+    </form>
+</body>
+</html>
+
+
